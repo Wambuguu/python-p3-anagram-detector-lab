51d0316c3df4cc9c25b078c9980dba025c4e47ec
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-22 11:18:49 +0300

    add

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        matches = []
+        sorted_word = sorted(self.word)
+        for anagram in possible_anagrams:
+            if self._is_anagram(anagram.lower(), sorted_word):
+                matches.append(anagram)
+        return matches
+
+    def _is_anagram(self, candidate, sorted_word):
+        if candidate == self.word:
+            return False
+        return sorted(candidate) == sorted_word
